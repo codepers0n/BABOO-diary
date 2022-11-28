359dc053cddba20c100b46732a91c699bb9985ee
codepers0n <ssu55555@knou.ac.kr>
2022-11-28 18:48:36 +0900

    BABOO

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hellp <code>BABOO</code> world
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/codepers0n/BABOO-diary"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          mung mung! 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
